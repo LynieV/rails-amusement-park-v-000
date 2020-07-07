617001fb0e8086d1a36d6cfd312e37bbc6371d6b
Lynie McClimon <vmcclimon33@yahoo.com>
2020-07-07 23:38:06 +0000

    Automatically backed up by Learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,69 +1,35 @@
 class AttractionsController < ApplicationController
-  before_action :set_attraction, only: [:show, :edit, :update, :destroy]
-
-  # GET /attractions
   def index
     @attractions = Attraction.all
   end
 
-  # GET /attractions/1
   def show
+    @attraction = Attraction.find_by(id: params[:id])
+    @ride = @attraction.rides.build(user_id: current_user.id)
   end
 
-  # GET /attractions/new
   def new
     @attraction = Attraction.new
   end
 
-  # GET /attractions/1/edit
-  def edit
-  end
-
-  # POST /attractions
   def create
-    @attraction = Attraction.new(attraction_params)
-
-    respond_to do |format|
-      if @attraction.save
-        format.html { redirect_to @attraction, notice: 'Attraction was successfully created.' }
-        format.json { render :show, status: :created, location: @attraction }
-      else
-        format.html { render :new }
-        format.json { render json: @attraction.errors, status: :unprocessable_entity }
-      end
-    end
+    attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(attraction)
   end
 
-  # PATCH/PUT /attractions/1
+  def edit
+    @attraction = Attraction.find_by(id: params[:id])
+    @ride = @attraction.rides.build(user_id: current_user.id)
+  end
+
   def update
-    respond_to do |format|
-      if @attraction.update(attraction_params)
-        format.html { redirect_to @attraction, notice: 'Attraction was successfully updated.' }
-        format.json { render :show, status: :ok, location: @attraction }
-      else
-        format.html { render :edit }
-        format.json { render json: @attraction.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /attractions/1
-  def destroy
-    @attraction.destroy
-    respond_to do |format|
-      format.html { redirect_to attractions_url, notice: 'Attraction was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    attraction = Attraction.find_by(id: params[:id])
+    attraction.update(attraction_params)
+    redirect_to attraction_path(attraction)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_attraction
-      @attraction = Attraction.find_by(id: params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def attraction_params
-      params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
+      params.require(:attraction).permit(:name, :min_height, :tickets, :happiness_rating, :nausea_ratiing)
     end
 end
