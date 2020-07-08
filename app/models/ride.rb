4f28aec2e0ca524bb8a73f571e6b9c9f720f1b03
Lynie McClimon <vmcclimon33@yahoo.com>
2020-07-08 00:13:18 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,23 +1,21 @@
 class Ride < ActiveRecord::Base
-  belongs_to :user
   belongs_to :attraction
+  belongs_to :user
 
   def take_ride
-    attraction = self.attraction
-    user = self.user
-
     if attraction.tickets > user.tickets && attraction.min_height > user.height
+      # The user doesn't have enough tickets and isn't tall enough to ride the attraction
       "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
     elsif attraction.tickets > user.tickets
+      # The user doesn't have enough tickets to ride the attraction
       "Sorry. You do not have enough tickets to ride the #{attraction.name}."
     elsif attraction.min_height > user.height
+      # The user isn't tall enough to ride the attraction
       "Sorry. You are not tall enough to ride the #{attraction.name}."
-    else
-      current_tickets = user.tickets - attraction.tickets
-      current_nausea = user.nausea + attraction.nausea_rating
-      current_happiness = user.happiness + attraction.happiness_rating
-
-      user.update(tickets: current_tickets, nausea: current_nausea, happiness: current_happiness)
-   end
+    else # The user rides the attraction
+      # Update the user's number of tickets, nausea, and happiness
+      user.update(tickets: user.tickets -= attraction.tickets, nausea: user.nausea += attraction.nausea_rating, happiness: user.happiness += attraction.happiness_rating)
+      "Thanks for riding the #{attraction.name}!"
+    end
   end
 end
